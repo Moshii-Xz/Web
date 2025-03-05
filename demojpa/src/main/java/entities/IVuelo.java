658b51749e0caeb433c90761fc114cdebc370c9a
Moshii <MauricioYT102@gmail.com>
2025-03-04 21:52:40 -0500

    Agregando los @Query Methods

--- demojpa/src/main/java/entities/IVuelo.java
+++ demojpa/src/main/java/entities/IVuelo.java
@@ -1,6 +1,7 @@
 package entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface IVuelo extends JpaRepository<Vuelo, Long> {
@@ -11,4 +12,15 @@ public interface IVuelo extends JpaRepository<Vuelo, Long> {
     Vuelo findByReservasPasajeroNid(String nid);
     Vuelo findByReservasCodigo(String codigo);
 	
+
+    @Query("SELECT v FROM Vuelo v WHERE v.aerolinea.nombre = ?1")
+        Vuelo findByAerolineaNombre(String nombre);
+    @Query("SELECT v FROM Vuelo v WHERE v.reservas.pasajero.nombre = ?1")
+        Vuelo findByReservasPasajeroNombre(String nombre);
+    @Query("SELECT v FROM Vuelo v WHERE v.reservas.pasajero.nid = ?1")
+        Vuelo findByReservasPasajeroNid(String nid);
+    @Query("SELECT v FROM Vuelo v WHERE v.reservas.codigo = ?1")
+        Vuelo findByReservasCodigo(String codigo);
+    @Query("SELECT v FROM Vuelo v WHERE v.destino = ?1")
+        Vuelo findByDestino(String destino);
 }
